2939381956b7192af44714f3d06372194c84212a
nfottz <yblee122077@gmail.com>
2023-04-21 09:46:56 +0900

    2023-04-21

--- 07_Mybatis_MockMvc/src/main/java/com/gdu/app07/config/DBConfig.java
+++ 07_Mybatis_MockMvc/src/main/java/com/gdu/app07/config/DBConfig.java
@@ -11,12 +11,15 @@ import org.springframework.core.env.Environment;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 // application.properties 파일의 속성을 읽어 오자!
-@PropertySource(value = {"classpath:application.properties"})	
+@PropertySource(value = {"classpath:application.properties"})
+// 트랜잭션 처리 허용하기
+@EnableTransactionManagement
 @Configuration
 public class DBConfig {
 
@@ -36,15 +39,15 @@ public class DBConfig {
 	
 	// HikariDataSource Bean
 	@Bean(destroyMethod = "close")
-	public HikariDataSource dataSource() {
+	public HikariDataSource hikariDataSource() {
 		return new HikariDataSource(hikariConfig());
 	}
 	
 	// SqlSessionFactory Bean
 	@Bean
-	public SqlSessionFactory factory() throws Exception {
+	public SqlSessionFactory sqlSessionFactory() throws Exception {
 		SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
-		bean.setDataSource(dataSource());
+		bean.setDataSource(hikariDataSource());
 		bean.setConfigLocation(new PathMatchingResourcePatternResolver().getResource(env.getProperty("mybatis.config-location")));
 		bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(env.getProperty("mybatis.mapper-locations")));
 		return bean.getObject();
@@ -52,14 +55,14 @@ public class DBConfig {
 	
 	// SqlSessionTemplate Bean (기존의 SqlSession)
 	@Bean
-	public SqlSessionTemplate template() throws Exception {
-		return new SqlSessionTemplate(factory());
+	public SqlSessionTemplate sqlSessionTemplate() throws Exception {
+		return new SqlSessionTemplate(sqlSessionFactory());
 	}
 	
 	// TransactionManager Bean
 	@Bean
 	public TransactionManager transactionManager() {
-		return new DataSourceTransactionManager(dataSource());
+		return new DataSourceTransactionManager(hikariDataSource());
 	}
 	
 }
