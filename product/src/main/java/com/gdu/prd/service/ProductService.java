ae632a21ac14b29aca967375c63191f5fc56da61
nfottz <yblee122077@gmail.com>
2023-04-25 20:26:59 +0900

    2023-04-25

---
+++ product/src/main/java/com/gdu/prd/service/ProductService.java
@@ -0,0 +1,13 @@
+package com.gdu.prd.service;
+
+import org.springframework.ui.Model;
+
+import com.gdu.prd.domain.ProductDTO;
+
+public interface ProductService {
+
+	public void loadProductList(Model model);
+	public int addProduct(ProductDTO productDTO);
+//	public int addProduct(String prodName, int prodPrice);
+	
+}
